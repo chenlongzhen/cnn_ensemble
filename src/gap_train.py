125aa478b900131278ddce92a7b1ad3d59e3e234
chenlongzhen <chenlongzhen@autohome.com.cn>
2017-04-21 01:37:31 +0000

    init

---
+++ src/gap_train.py
@@ -0,0 +1,178 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import h5py
+import sys,os
+import functools
+
+import logging
+import keras
+import numpy as np
+from keras.models import save_model
+from sklearn.utils import shuffle
+from keras.callbacks import TensorBoard,EarlyStopping,CSVLogger,ModelCheckpoint
+import yaml
+
+def myLog(logPath):
+    '''
+    logging
+    :param logPath:  where to save log
+    :return: logging handle
+    '''
+
+    logging.basicConfig(level=logging.INFO,
+                        format='[ %(asctime)s %(filename)s line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename=logPath,
+                        filemode='w')
+
+    #定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+    return logging
+
+
+#################################
+# cofig
+#################################
+CNF = yaml.load(open('../conf/setting.yaml'))
+
+version = CNF['version']
+
+prefix = CNF['prefix']
+
+test_path = CNF['test_path']
+
+use_model = CNF['use_model']
+
+gen_layer = CNF['gen_layer']
+
+# board
+board_path = prefix + '/'+CNF['board_path']
+if not os.path.isdir(board_path): os.mkdir(board_path)
+
+# train log
+LOG_PATH = prefix + '/'+CNF['train_log']
+if not os.path.isdir(LOG_PATH): os.mkdir(LOG_PATH)
+
+# weight path
+BEST_WEIGHT = prefix + '/'+CNF['weight_path']
+if not os.path.isdir(BEST_WEIGHT): os.mkdir(BEST_WEIGHT)
+
+# logger path
+log_path = prefix + '/' + CNF['log_path']
+if not os.path.isdir(log_path): os.mkdir(log_path)
+
+# data shuffle seed
+np.random.seed(int(CNF['seed']))
+
+# logger
+logger = myLog(log_path+"/log_{}".format(version))
+logger.info(CNF)
+
+# train settings
+batch_size = int(CNF['batch_size'])
+nb_epoch = int(CNF['nb_epoch'])
+validation_split=float(CNF['validation_split'])
+
+# fixed path
+submissionPath =  "../data/output/submission.csv"
+END_MODEL =  "../data/endModel/endModel_{}.h5".format(version)
+model_path = '../data/model'
+end_model_path = '../data/endModel'
+if not os.path.isdir(model_path): os.mkdir(model_path)
+if not os.path.isdir(end_model_path): os.mkdir(end_model_path)
+
+# topk_acc
+topK_acc = CNF['topK_acc']
+
+#################################
+# read features
+#################################
+
+X_train = []
+X_test = []
+
+for m in use_model:
+    filename =  "../data/model/gap_{}_{}.h5".format(m,version)
+    logger.info("[INFO] begin to read {}".format(os.path.basename(filename)))
+    with h5py.File(filename, 'r') as h:
+        X_train.append(np.array(h['train']))
+        X_test.append(np.array(h['test']))
+        y_train = np.array(h['label'])
+
+X_train = np.concatenate(X_train, axis=1)
+X_test = np.concatenate(X_test, axis=1)
+
+logger.info("train shape\r")
+logger.info(X_train.shape)
+logger.info("test shape\r")
+logger.info(X_test.shape)
+
+logger.info("[INFO] shuffle")
+X_train, y_train = shuffle(X_train, y_train)
+
+#################################
+# train pipe
+#################################
+
+from keras.models import *
+from keras.layers import *
+
+logger.info("[INFO] train")
+input_tensor = Input(X_train.shape[1:])
+x = input_tensor
+x = Dropout(0.5)(x)
+x = Dense(1, activation='sigmoid')(x)
+model = Model(input_tensor, x)
+model.summary()
+
+topK_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=5)
+
+topK_acc.__name__ = 'topK_acc'
+
+model.compile(optimizer='adadelta',
+              loss='binary_crossentropy',
+              metrics=['accuracy',topK_acc])
+
+
+##############################
+# Call Back
+##############################
+
+# tensor board
+tb = TensorBoard(log_dir=board_path+'/{}/'.format(version), histogram_freq=1,  
+                  write_graph=True, write_images=False)
+#* tensorboard --logdir path_to_current_dir/Graph --port 8080 
+print("tensorboard --logdir {} --port 8080".format(board_path))
+
+
+# earlystoping
+# ES = EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto')
+
+# csv log
+csvlog = CSVLogger(LOG_PATH+"/{}.log".format(version),separator=',', append=True)
+
+# saves the model weights after each epoch if the validation loss decreased
+checkpointer = ModelCheckpoint(filepath=BEST_WEIGHT+"/{}.h5".format(version),verbose=1, save_best_only=True)
+
+##############################
+# fit
+##############################
+model.fit(X_train, y_train, batch_size=batch_size, epochs=nb_epoch, validation_split=validation_split,
+        callbacks = [tb,csvlog,checkpointer])
+
+logger.info("[INFO] save model")
+save_model(model,END_MODEL)
+
+
+##yaml test
+#with open('../data/model.yaml','w') as ifile:
+#    ifile.write(model.to_yaml())
+
